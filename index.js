0fbaccaabf91fc1b1e24714f8f88c1420aa14f02
Raiane <raianemachadonascimento@gmail.com>
2023-12-01 19:45:08 -0300

    salvar

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function mudarCor(invertido) {
     // Inverte as cores se "invertido" for verdadeiro, caso contrário, restaura para a cor original
     minhaImagem.style.filter = invertido ? 'invert(100%)' : 'invert(0%)';
 }
+
+  // Função para mudar a imagem de fundo
+  function mudarImagemDeFundo() {
+    document.body.style.backgroundImage = "url('C:\Users\Raiane\Documents\Documentos\Site Raiane Machado-4°info\Capa..png')";
+}
+
+// Chame a função quando a página carregar ou em algum evento específico
+window.onload = mudarImagemDeFundo;
